9dc457e2df91b15213f5da2002af963ed7b19f84
CrizJimz <c.chrisjim@gmail.com>
2023-11-07 16:22:45 -0600

    Codigo Ejericio 1

---
+++ 50-Ejercicios/Ejercicio1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int Num1;
+	int Num2;
+	int resultado;
+	
+	cout<<"Ingrese los valores a operar\n";
+	cout<<"Primer valor: ";
+	cin>>Num1;
+	cout<<"Segundo valor: ";
+	cin>>Num2;
+	resultado=Num1+Num2;
+	cout<<"La suma de estos valores es "<<resultado<<endl;
+	resultado=Num1-Num2;
+	cout<<"La resta de estos valores es "<<resultado<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
